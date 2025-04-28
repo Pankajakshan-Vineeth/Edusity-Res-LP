55af04720ebeaec8f65ee3b0901f4164b6264075
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-04-28 20:32:08 +0530

    added hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import './Hero.css'; // Don't forget to import your CSS!
+import './Hero.css'; 
+import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <h1>Welcome to Edusity!</h1>
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>We Ensure better education for a better world</h1>
+        <p>Our cutting edge curriculum is designed to enpower students
+          with the knowledge, skills and experiences to excel in 
+          dynamic field of education
+        </p>
+        <button className="btn">Explore More <img src={dark_arrow} alt="" /></button>
+      </div>
     </div>
   );
 }

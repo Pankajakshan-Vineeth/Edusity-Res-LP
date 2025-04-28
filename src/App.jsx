fbf22957853d7ad6f8aeaa834f644d0f56519e83
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-04-28 16:06:56 +0530

    nav bar and bg image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello Vineeth</h1>
+    <Navbar/>
+    <Hero/>
     </div>
   )
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hero.css'; // Don't forget to import your CSS!
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>Welcome to Edusity!</h1>
+    </div>
+  );
+}
+
+export default Hero;

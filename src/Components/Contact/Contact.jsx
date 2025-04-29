54d5f0ded799ce07c911c71b9b0199d38ff62734
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-04-29 11:18:56 +0530

    contact section completed

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,15 +1,47 @@
 import React from "react";
 import "./Contact.css";
-import msg_icon from '../../assets/msg-icon.png'
-import mail_icon from '../../assets/mail-icon.png'
-import phone_icon from '../../assets/phone-icon.png'
-import location_icon from '../../assets/location-icon.png'
+import msg_icon from "../../assets/msg-icon.png";
+import mail_icon from "../../assets/mail-icon.png";
+import phone_icon from "../../assets/phone-icon.png";
+import location_icon from "../../assets/location-icon.png";
+import white_arrow from '../../assets/white-arrow.png'
+
+
 
 const Contact = () => {
+
+    const [result, setResult] = React.useState("");
+
+      
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "30117c7c-fa97-483b-a08c-ec904000ca70");
+    
+        const response = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          body: formData
+        });
+    
+        const data = await response.json();
+    
+        if (data.success) {
+          setResult("Form Submitted Successfully");
+          event.target.reset();
+        } else {
+          console.log("Error", data);
+          setResult(data.message);
+        }
+      };
+
   return (
-    <div className="Contact">
+    <div className="contact">
       <div className="contact-col">
-        <h3>Send us a message <img src={msg_icon} alt="" /></h3>
+        <h3>
+          Send us a message <img src={msg_icon} alt="" />
+        </h3>
         <p>
           Feel free to reach out through contact form or find our contact
           information below. Your feedback, questions, and suggestions are
@@ -17,14 +49,45 @@ const Contact = () => {
           university community.
         </p>
         <ul>
-          <li><img src = {mail_icon} alt="" />Contact@GreatStack.dev</li>
-          <li><img src={phone_icon} alt="" />+1 123-456-7890</li>
-          <li><img src= {location_icon} alt="" />77 Massachusetts Ave, Cambridge
+          <li>
+            <img src={mail_icon} alt="" />
+            Contact@GreatStack.dev
+          </li>
+          <li>
+            <img src={phone_icon} alt="" />
+            +1 123-456-7890
+          </li>
+          <li>
+            <img src={location_icon} alt="" />
+            77 Massachusetts Ave, Cambridge
             <br />
-            MA 02139, United States</li>
+            MA 02139, United States
+          </li>
         </ul>
       </div>
-      <div className="contact-col"></div>
+      <div className="contact-col">
+        <form onSubmit = {onSubmit}>
+          <label>Your name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter your mobile number"
+            required
+          />
+          <label>Write Your Message Here</label>
+          <textarea name="message" rows = '6' placeholder="Enter Your Message" required></textarea>
+          <button type = 'submit' className="btn dark-btn">Submit Now <img src={white_arrow} alt="" /></button>
+        </form>
+        <span>{result}</span>
+      </div>
     </div>
   );
 };
